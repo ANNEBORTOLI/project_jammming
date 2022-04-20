42c52662cdddc5e3154bfbc1d04d39853fb138ae
Anne Bortoli <annebortoli@gmail.com>
2022-04-20 19:13:55 -0300

    :test_tube:feat: testing SearchResults component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './TrackList.css';
-import '../Track/Track';
+import Track from '../Track/Track';
 
 
 class TrackList extends React.Component {
   render () {
     return (
     <div className="TrackList">
-    <Track />
+      {
+      
+       this.props.tracks.map(track => {
+          return <Track track={track} key={track.id} />
+        })
+      }
     </div>
     )
   }
