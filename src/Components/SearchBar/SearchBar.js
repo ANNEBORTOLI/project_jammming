7f24425e83aaeec2c1952bb3db6a764350aaa3cc
Anne Bortoli <annebortoli@gmail.com>
2022-04-25 09:42:50 -0300

    :rocket:feat: save a user's playlist on spotify

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,13 @@
-import React from 'react';
- import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      term: ''
-    }
+      term: "",
+    };
 
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
@@ -21,13 +21,18 @@ class SearchBar extends React.Component {
     this.setState({ term: e.target.value });
   }
 
-  render () {
+  render() {
     return (
-        <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
-            <button className="SearchButton">SEARCH</button>
+      <div className="SearchBar">
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+        />
+        <button className="SearchButton" onClick={this.search}>
+          SEARCH
+        </button>
       </div>
-    )
+    );
   }
 }
 
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,5 @@
-require("dotenv-safe").config();
-const { CLIENT_ID, REDIRECT_URI } = process.env;
-
-const clientId = CLIENT_ID;
-const redirectUri = REDIRECT_URI;
+const clientId = "YOUR USER ID";
+const redirectUri = "YOUR URI";
 
 let accessToken;
 
@@ -50,6 +47,39 @@ const Spotify = {
           album: track.album.name,
           uri: track.uri,
         }));
+      });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch(`https://api.spotify.com/v1/me`, { headers: headers })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        userId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({ name: name }),
+        })
+          .then((response) => response.json())
+          .then((jsonResponse) => {
+            const playlistId = jsonResponse.id;
+            return fetch(
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+              {
+                headers: headers,
+                method: "POST",
+                body: JSON.stringify({ uris: trackUris }),
+              }
+            );
+          });
       });
   },
 };
